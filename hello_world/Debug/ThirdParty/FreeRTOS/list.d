f558e6660d053f0659e6dd855e47d0135ea7bb91
ashwinpillai45 <ashwin45@gmail.com>
2021-10-22 02:05:29 -0400

    Added code to send message via ITM using freeRTOS. 2 tasks created

---
+++ hello_world/Debug/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/list.o: ../ThirdParty/FreeRTOS/list.c \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/portable.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/list.h
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/projdefs.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/portable.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/list.h:
---
+++ hello_world/Debug/ThirdParty/FreeRTOS/stream_buffer.d
@@ -0,0 +1,32 @@
+ThirdParty/FreeRTOS/stream_buffer.o: \
+ ../ThirdParty/FreeRTOS/stream_buffer.c \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/portable.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/task.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/list.h \
+ /home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/stream_buffer.h
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/projdefs.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/portable.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/task.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/list.h:
+
+/home/ashwin/Documents/rtos/rtos/hello_world/ThirdParty/FreeRTOS/include/stream_buffer.h:
